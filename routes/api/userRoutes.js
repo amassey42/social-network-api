7f2e45b660b2267ec0ce7a6ee5da1a1696ef8784
andrew <ammassey42@gmail.com>
2022-11-27 15:55:09 -0800

    creaetd user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 const users = require('../../controllers/userController');
 
+//get all users
+router.get('/', users.getUsers);
+//get one user
+router.get('/:id', users.getOneUser);
+//create a user
+router.post('/', users.createUser);
+//update a user
+router.put('/:id', users.updateUser);
+//delete a user
+router.delete('/:id', users.deleteUser);
+//add a friend
+router.post('/:userId/friends/:friendId', users.addFriend);
+//delete a friend
+router.delete('/:userId/friends/:friendId', users.deleteFriend);
 module.exports = router;
