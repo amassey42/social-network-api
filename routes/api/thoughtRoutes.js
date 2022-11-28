7955595ff9a43083aa604baeba5a7e1c4e6a2dfb
andrew <ammassey42@gmail.com>
2022-11-27 16:00:02 -0800

    added thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
 const thoughts = require('../../controllers/thoughtsController');
 
+//get all thoughts
+router.get('/', thoughts.getThoughts);
+//get one thought
+router.get('/:id', thoughts.getOneThought);
+//create a thought
+router.post('/', thoughts.createThought);
+//update a thought
+router.put('/:id', thoughts.updateThought);
+//delete a thought
+router.delete('/:id', thoughts.deleteThought);
+//add a reaction
+router.post('/:id/reactions', thoughts.createReaction);
+//delete a reaction
+router.delete('/:thoughtId/reactions/:reactionId', thoughts.deleteReaction);
 module.exports = router;
